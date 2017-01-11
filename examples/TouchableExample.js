9c0b440c8accfcf9202ca0f292f800032979b5c0
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-01-11 13:44:54 +0100

    update touchable example to cover more cases

--- examples/TouchableExample.js
+++ examples/TouchableExample.js
@@ -12,52 +12,79 @@ import Menu, {
   MenuTrigger,
 } from 'react-native-popup-menu';
 
-const TouchableExample = () => (
-  <MenuContext style={{flexDirection: 'column', padding: 30}}>
+class TouchableExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Touchable: Button
+    };
+  }
 
-    <Menu>
-      <MenuTrigger
-        customStyles={{
-          TriggerTouchableComponent: Button,
-          triggerTouchable: { title: 'Select (Custom Touchables)' }
-        }}
-      />
-      <MenuOptions>
-        <MenuOption text='Default' />
-        <MenuOption text='TouchableOpacity' customStyles={{
-          OptionTouchableComponent: TouchableOpacity,
-          optionTouchable: touchableOpacityProps,
-        }} />
-        <MenuOption text='TouchableHighlight' customStyles={{
-          OptionTouchableComponent: TouchableHighlight,
-          optionTouchable: touchableHighlightProps,
-        }} />
-        <MenuOption text='TouchableWithoutFeedback' customStyles={{
-          OptionTouchableComponent: TouchableWithoutFeedback,
-        }} />
-      </MenuOptions>
-    </Menu>
+  render() {
+    const { Touchable } = this.state;
+    const buttonText = 'Select ' + (Touchable ? (getDisplayName(Touchable)) : 'default');
+    return (
+      <MenuContext style={{flexDirection: 'column', padding: 30}}>
 
-    <Menu style={{paddingTop: 30}}>
-      <MenuTrigger
-        customStyles={{
-          TriggerTouchableComponent: Button,
-          triggerTouchable: { title: 'Select (all TouchableOpacity)' }
-        }}
-      />
-      <MenuOptions customStyles={{
-        OptionTouchableComponent: TouchableOpacity,
-        optionTouchable: touchableOpacityProps,
-      }}>
-        <MenuOption text='Option 1' />
-        <MenuOption text='Option 2' />
-        <MenuOption text='Option 3' />
-        <MenuOption text='Option 4' />
-      </MenuOptions>
-    </Menu>
+        <Menu onSelect={Touchable => this.setState({ Touchable })}>
+          <MenuTrigger
+            customStyles={{
+              TriggerTouchableComponent: Button,
+              triggerTouchable: { title: 'Select (Custom Touchables)' }
+            }}
+          />
+        <MenuOptions>
+            <MenuOption text='Default' />
+            <MenuOption text='TouchableOpacity' customStyles={{
+                OptionTouchableComponent: TouchableOpacity,
+                optionTouchable: touchableOpacityProps,
+              }}
+              value={TouchableOpacity}
+            />
+            <MenuOption text='TouchableHighlight' customStyles={{
+                OptionTouchableComponent: TouchableHighlight,
+                optionTouchable: touchableHighlightProps,
+              }}
+              value={TouchableHighlight}
+            />
+            <MenuOption text='TouchableWithoutFeedback' customStyles={{
+              OptionTouchableComponent: TouchableWithoutFeedback,
+              }}
+              value={TouchableWithoutFeedback}
+            />
+            <MenuOption customStyles={{
+                OptionTouchableComponent: Button,
+                optionTouchable: { title: 'Button' }
+              }}
+              value={Button}
+            />
+          </MenuOptions>
+        </Menu>
 
-  </MenuContext>
-);
+        <Menu style={{paddingTop: 30}}>
+          <MenuTrigger
+            customStyles={{
+              TriggerTouchableComponent: Touchable,
+              triggerTouchable: { title: buttonText }
+            }}
+            text={buttonText}
+          />
+          <MenuOptions customStyles={{
+            OptionTouchableComponent: TouchableOpacity,
+            optionTouchable: touchableOpacityProps,
+          }}>
+            <MenuOption text='Option 1' />
+            <MenuOption text='Option 2' />
+            <MenuOption text='Option 3' />
+            <MenuOption text='Option 4' />
+          </MenuOptions>
+        </Menu>
+
+      </MenuContext>
+    );
+
+  }
+}
 
 const touchableOpacityProps = {
   activeOpacity: 0.6,
@@ -67,5 +94,11 @@ const touchableHighlightProps = {
   activeOpacity: 0.5,
   underlayColor: 'green',
 };
+
+const getDisplayName = Component => (
+  Component.displayName ||
+  Component.name ||
+  (typeof Component === 'string' ? Component : 'Component')
+);
 
 export default TouchableExample;
