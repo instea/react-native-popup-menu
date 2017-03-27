e2eb557f5ee86ab378e933de46e8d9ddf13efe33
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-03-27 23:15:32 +0200

    fix compatibility with older RN versions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// platform select polyfil for older RN versions
+import { Platform } from 'react-native';
+if (!Platform.select) {
+  Platform.select = (obj) => obj[Platform.OS];
+}
+
 import Menu from './Menu';
 import MenuContext from './MenuContext';
 import MenuOption from './MenuOption';
