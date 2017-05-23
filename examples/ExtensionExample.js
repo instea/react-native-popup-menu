3aa96019c29bb40bb555edafc9ca2af77de29260
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-05-23 23:05:52 +0200

    Add Icon extension example

--- examples/ExtensionExample.js
+++ examples/ExtensionExample.js
@@ -7,9 +7,19 @@ import Menu, {
   MenuTrigger,
   renderers,
 } from 'react-native-popup-menu';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const CheckedOption = (props) => (
   <MenuOption {...props} text={(props.checked ? '\u2713 ' : '') + props.text} />
+)
+
+const IconOption = ({iconName, text, ...others}) => (
+  <MenuOption {...others} >
+    <Text>
+      <Icon name={iconName} />
+      {' ' + text}
+    </Text>
+  </MenuOption>
 )
 
 /* You can set default renderer for all menus just once in your application: */
@@ -26,6 +36,7 @@ const ExtensionExample = () => (
       <MenuOptions>
         <CheckedOption value={1} text='One' />
         <CheckedOption checked value={2} text='Two' />
+        <IconOption value={3} iconName='rocket' text='Three' />
       </MenuOptions>
     </Menu>
   </MenuContext>
