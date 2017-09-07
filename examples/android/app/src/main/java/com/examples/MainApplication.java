02d55275c2fca76798a7ebe8e67bfe1b18755dda
Stanislav Miklik <stanislav.miklik@gmail.com>
2017-09-07 10:42:40 +0200

    upgrade native projects to newer RN

--- examples/android/app/src/main/java/com/examples/MainApplication.java
+++ examples/android/app/src/main/java/com/examples/MainApplication.java
@@ -3,12 +3,11 @@ package com.examples;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
-import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,7 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new VectorIconsPackage()
+            new VectorIconsPackage()
       );
     }
   };
