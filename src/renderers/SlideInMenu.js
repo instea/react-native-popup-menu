5cf71cc2fba43b495b55a6fc8ce4d60bee9b26f6
Martin Bielik <mx.bielik@gmail.com>
2017-12-21 14:25:26 +0100

    closing slide-in menu by pull out

--- src/renderers/SlideInMenu.js
+++ src/renderers/SlideInMenu.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Animated, StyleSheet, Easing } from 'react-native';
+import PropTypes from 'prop-types';
+import { View, PanResponder, Animated, StyleSheet, Easing } from 'react-native';
 import { OPEN_ANIM_DURATION, CLOSE_ANIM_DURATION } from '../constants';
 
 export const computePosition = ({ windowLayout, optionsLayout }) => {
@@ -14,9 +15,11 @@ export default class SlideInMenu extends React.Component {
 
   constructor(props) {
     super(props);
+    this.slideValue = 0;
     this.state = {
-      slide: new Animated.Value(0),
+      slide: new Animated.Value(this.slideValue),
     };
+    this.state.slide.addListener(this.onSlideAnimation);
   }
 
   componentDidMount() {
@@ -28,7 +31,18 @@ export default class SlideInMenu extends React.Component {
     }).start();
   }
 
+  componentWillUnmount() {
+    this.state.slide.removeListener(this.onSlideAnimation);
+  }
+
+  onSlideAnimation = ({ value }) => {
+    this.slideValue = value;
+  }
+
   close() {
+    if (this.slideValue === 0) {
+      return;
+    }
     return new Promise(resolve => {
       Animated.timing(this.state.slide, {
         duration: CLOSE_ANIM_DURATION,
@@ -53,12 +67,57 @@ export default class SlideInMenu extends React.Component {
     };
     const position = computePosition(layouts);
     return (
-      <Animated.View style={[styles.options, { width }, style, animation, position]} {...other}>
-        {children}
-      </Animated.View>
+      <View
+        {...this.panResponder.panHandlers}
+        style={styles.panResponder}
+        collapsable={false}
+      >
+        <Animated.View
+          style={[styles.options, { width }, style, animation, position]}
+          {...other}
+        >
+          {children}
+        </Animated.View>
+      </View>
     );
   }
+
+  panResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onMoveShouldSetPanResponder: () => false, // don't suppress menu options handlers
+    onPanResponderMove: (evt, gestureState) => {
+      const { height } = this.props.layouts.optionsLayout;
+      const newValue = Math.max(Math.min(1 - gestureState.dy / height, 1), 0);
+      this.state.slide.setValue(newValue);
+    },
+    onPanResponderRelease: (evt, gestureState) => {
+      if (gestureState.vy < 0) {
+        // open
+        Animated.timing(this.state.slide, {
+          duration: (1 - this.slideValue) * OPEN_ANIM_DURATION,
+          toValue: 1,
+          easing: Easing.out(Easing.cubic),
+          useNativeDriver: true
+        }).start();
+      } else {
+        // close
+        Animated.timing(this.state.slide, {
+          duration: this.slideValue * OPEN_ANIM_DURATION,
+          toValue: 0,
+          easing: Easing.out(Easing.cubic),
+          useNativeDriver: true
+        }).start(() => {
+          this.context.menuActions.closeMenu();
+        });
+      }
+    },
+  })
+
 }
+
+SlideInMenu.contextTypes = {
+  menuActions: PropTypes.object,
+};
 
 const styles = StyleSheet.create({
   options: {
@@ -73,5 +132,12 @@ const styles = StyleSheet.create({
 
     // This will elevate the view on Android, causing shadow to be drawn.
     elevation: 5,
+  },
+  panResponder: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
