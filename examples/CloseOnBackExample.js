92123fbf84da7c155c61859c3a660f9aca2a7b37
Stanislav Miklik <stanislav.miklik@gmail.com>
2022-08-22 23:19:54 +0200

    upgrade expo and back example

--- examples/CloseOnBackExample.js
+++ examples/CloseOnBackExample.js
@@ -1,41 +1,70 @@
-import React, { Component } from 'react';
-import { Text, Button } from 'react-native';
+import React, { Component } from "react";
+import { Text, Button } from "react-native";
 import {
   Menu,
   MenuProvider,
   MenuOptions,
   MenuOption,
   MenuTrigger,
-} from 'react-native-popup-menu';
+} from "react-native-popup-menu";
 
 class CloseOnBackExample extends Component {
   state = {
     customBackHandler: false,
-  }
+    additionalMenu: false,
+  };
 
   customBackHandler = (instance) => {
-    alert(`Back button was pressed. Current menu state: ${instance.isMenuOpen() ? 'opened' : 'closed'}`);
+    alert(
+      `Back button was pressed. Current menu state: ${
+        instance.isMenuOpen() ? "opened" : "closed"
+      }`
+    );
     return true;
-  }
+  };
 
   render() {
+    const { additionalMenu } = this.state;
     return (
       <MenuProvider
-        style={{flexDirection: 'column', padding: 30}}
-        backHandler={this.state.customBackHandler ? this.customBackHandler : true}>
+        style={{ flexDirection: "column", padding: 50 }}
+        backHandler={
+          this.state.customBackHandler ? this.customBackHandler : true
+        }
+      >
+        <Button
+          title={
+            this.state.customBackHandler
+              ? "Change to default"
+              : "Change to custom"
+          }
+          onPress={() =>
+            this.setState({ customBackHandler: !this.state.customBackHandler })
+          }
+        />
         <Menu>
-          <MenuTrigger text='Select option' />
+          <MenuTrigger text="Select option" />
           <MenuOptions>
-            <MenuOption value={1} text='One' />
+            <MenuOption value={1} text="One" />
             <MenuOption value={2}>
-              <Text style={{color: 'red'}}>Two</Text>
+              <Text style={{ color: "red" }}>Two</Text>
             </MenuOption>
-            <MenuOption value={3} disabled={true} text='Three' />
+            <MenuOption value={3} disabled={true} text="Three" />
           </MenuOptions>
         </Menu>
         <Button
-          title={this.state.customBackHandler ? "Change to default" : "Change to custom"}
-          onPress={() => this.setState({ customBackHandler: !this.state.customBackHandler })}/>
+          title={additionalMenu ? "Remove 2nd menu" : "Add 2nd menu"}
+          onPress={() => this.setState({ additionalMenu: !additionalMenu })}
+        />
+        {additionalMenu && (
+          <Menu>
+            <MenuTrigger text="Select option 2" />
+            <MenuOptions>
+              <MenuOption value={1} text="One" />
+              <MenuOption value={2} text="Two" />
+            </MenuOptions>
+          </Menu>
+        )}
       </MenuProvider>
     );
   }
