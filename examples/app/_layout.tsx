d586720e1c8c35395b20fc5096ff5838bddd8b97
Stanislav Miklik <stanislav.miklik@gmail.com>
2025-12-13 23:28:14 +0100

    fix expo router header

--- examples/app/_layout.tsx
+++ examples/app/_layout.tsx
@@ -1,10 +1,7 @@
 import { Stack } from "expo-router";
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
-    <SafeAreaProvider>
-      <Stack />
-    </SafeAreaProvider>
+    <Stack screenOptions={{ headerShown: false }} />
   );
 }
