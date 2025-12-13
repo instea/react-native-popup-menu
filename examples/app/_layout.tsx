b77dd5c0ca446be5c99019f960b92764bfedbf55
Stanislav Miklik <stanislav.miklik@gmail.com>
2025-12-13 23:13:14 +0100

    upgrade to expo 54 - wrong layout WIP

--- examples/app/_layout.tsx
+++ examples/app/_layout.tsx
@@ -1,5 +1,10 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaProvider>
+      <Stack />
+    </SafeAreaProvider>
+  );
 }
